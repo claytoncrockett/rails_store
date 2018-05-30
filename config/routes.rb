e7d78b1fe0aba754a575995e493e79c1dcb37625
Clayton Crockett <claytoncrockett@gmail.com>
2018-05-30 16:36:35 -0600

    added another nest layer

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :departments do
     resources :items
   end
+
+  scope 'items/:item_id', as: 'item' do
+    resources :comments, only: [:new, :create]
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+
+  dept = Department.create(
+    name: Faker::Commerce.department(1)
+  )
+
+  20.times do
+    item = dept.items.create(
+      name: Faker::Commerce.product_name,
+      price: Faker::Commerce.price, 
+      description: Faker::Hacker.say_something_smart
+    )
+
+    10.times do
+      item.comments.create(
+        body: Faker::FamilyGuy.quote,
+        rating: Faker::Number.between(1, 10),
+        author: Faker::Name.name_with_middle
+      )
+
+    end
+    
+  end
+
+end
